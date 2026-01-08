3218952c6d7e7eeb243e514dfbbe0de72306bf08
Tomáš <xmistinat@stuba.sk>
2026-01-08 16:18:43 +0100

    Updates

--- assets/keyboard2-fPhnGtMI.js
+++ assets/keyboard2-fPhnGtMI.js
@@ -1,4 +1,4 @@
-import{w as t}from"./index-jb6Ls4D8.js";/*!
+import{w as t}from"./index-CZbFAZrs.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const r=()=>{if(t!==void 0)return t.Capacitor};/*!
  * (C) Ionic http://ionicframework.com - MIT License
