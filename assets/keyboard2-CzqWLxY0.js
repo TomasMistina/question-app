317cb93ce78b6f2e3ca50d584cb083042c06cc7e
Tomáš <xmistinat@stuba.sk>
2026-02-19 19:00:21 +0100

    Updates

--- assets/keyboard2-CzqWLxY0.js
+++ assets/keyboard2-CzqWLxY0.js
@@ -1,4 +1,4 @@
-import{w as t}from"./index-CmZHRBMH.js";/*!
+import{w as t}from"./index-aO5Cy2BL.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const r=()=>{if(t!==void 0)return t.Capacitor};/*!
  * (C) Ionic http://ionicframework.com - MIT License
